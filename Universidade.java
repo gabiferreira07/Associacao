9792034f0435a85f15337c0ed4d4839eb3a05577
Gabrielli Ferreira <gabriellirodriguesferreira@gmail.com>
2024-10-09 16:32:13 -0300

    Associação

---
+++ Universidade.java
@@ -0,0 +1,18 @@
+package Associacao;
+
+public class Universidade {
+	private String nome;
+	
+	public Universidade(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+}
